6b8b448bd8b6d56c908020679de72f718ee0ab6a
Kevin Gabinete <kgabinete08@gmail.com>
2017-11-08 23:01:48 -0800

    weather data

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const tired = document.getElementById('tired');
+fetch('https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202442047&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys').then(res => {
+  res.json().then(data=> {
+    const condition = document.getElementById('condition');
+    condition.innerHTML = `${data.query.results.channel.item.condition.text}`;
 
-tired.addEventListener('click', (e) => {
-  
+    const temp = document.getElementById('temp');
+    temp.innerHTML = `${data.query.results.channel.item.condition.temp}`;
+  });
+}).catch(err => {
+  console.error(err.message);
 });
